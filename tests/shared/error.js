4f2a818b04b8a7e9c2e79c31fa11039a55ed4d41
Nuno Job <nunojobpinto@gmail.com>
2011-09-02 01:38:09 +0100

    fixed tests

--- tests/shared/error.js
+++ tests/shared/error.js
@@ -9,7 +9,7 @@ tests.empty_error = function (callback) {
 
 tests.empty_error_ok = function (_,e) {
   assert.equal(e.message, "Unknown Error");
-  assert.equal(e.status_code, 500);
+  assert.equal(e['status-code'], 500);
   assert.equal(e.error, "unknown");
   assert.ok(typeof e.request === 'object');
 };
@@ -20,7 +20,7 @@ tests.error_412 = function (callback) {
 
 tests.error_412_ok = function (_,e) {
   assert.equal(e.message, "Precondition Failed");
-  assert.equal(e.status_code, 412);
+  assert.equal(e['status-code'], 412);
   assert.equal(e.error, "unknown");
   assert.ok(typeof e.request === 'object');
 };
