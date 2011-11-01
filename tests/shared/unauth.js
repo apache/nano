ff9d5597d483bbbb2f5ad6b840182f4e3b19410b
tarantoga <tarantoga@me.com>
2011-11-01 13:05:57 +0100

    auth & unauth tests done

--- tests/shared/unauth.js
+++ tests/shared/unauth.js
@@ -13,8 +13,10 @@ tests.auth_user = function (callback) {
     , "password_sha" : "fdb2b8f8ae582440fbd11786fd9afd90920b42a1"
     , "salt"         : "659b9645544dfc82124b7fb07a0bd5f9"
     }, function(err,user) {
-      nano.auth('pat', '123', function(e,b,h){ 
-        callback(e,user.rev,b,h); 
+      nano.auth('pat', '123', function(){
+        nano.unauth(function(e,b,h){
+          callback(e,user.rev,b,h);
+        });
       });
     });
 };
@@ -23,10 +25,8 @@ tests.auth_user_ok = function (err,rev,response,headers) {
   nano.use('_users').destroy('org.couchdb.user:pat',rev);
   this.t.notOk(err); 
   this.t.equal(headers['status-code'], 200); // header tests go here
-  this.t.equal(headers['set-cookie'][0].length, 85);  
-  this.t.equal(response['ok'], true); // response tests go here
-  this.t.equal(response['name'], "pat");
-  this.t.equal(response['roles'][0], 'tester');
+  this.t.equal(headers['set-cookie'][0].length, 41);  
+  this.t.equal(response['ok'], true); // response tests go here  
 };
 
 ensure(__filename,tests,module,process.argv[2]);
