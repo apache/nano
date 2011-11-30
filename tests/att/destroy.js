005febe70db68694ad644ed8716642c2fd133acc
Nuno Job <nunojobpinto@gmail.com>
2011-11-30 15:36:44 +0000

    trying out nocknock

--- tests/att/destroy.js
+++ tests/att/destroy.js
@@ -3,6 +3,19 @@ var ensure   = require('ensure')
   , nano     = require('../../nano')(cfg)
   , db_name  = require('../utils').db_name("att_de")
   , tests    = exports
+  , nock     = require('nock')
+  , couchdb  = nock(cfg.url)
+                 .put( '/' + db_name("a")
+                     , ''
+                     , { 'content-type': 'application/json'
+                       , "accept": 'application/json' })
+                 .reply(201, { ok: true },
+                   { location: 'http://nodejsbug.iriscouch.com/v061_att_dea'
+                   , date: 'Wed, 30 Nov 2011 15:21:58 GMT'
+                   , 'content-type': 'application/json'
+                   , 'cache-control': 'must-revalidate'
+                   , 'status-code': 201 
+                   })
   ;
 
 function db(i) { return nano.use(db_name(i)); }
@@ -21,6 +34,7 @@ tests.att_des_ok = function (e,b) {
   this.t.notOk(e);
   this.t.ok(b.ok);
   this.t.equal(b.id, "new");
+  couchdb.done();
 };
 
 ensure(__filename,tests,module,process.argv[2]);
