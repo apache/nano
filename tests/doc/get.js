0193507fe7f14699c948888c65f604661eac801c
Nuno Job <nunojobpinto@gmail.com>
2011-12-02 19:36:05 +0000

    [nock] doc/get

--- tests/doc/get.js
+++ tests/doc/get.js
@@ -1,8 +1,70 @@
 var ensure   = require('ensure')
+  , nock     = require('nock')
   , cfg      = require('../../cfg/tests.js')
   , nano     = require('../../nano')(cfg)
   , db_name  = require('../utils').db_name("doc_ge")
-  , tests    = exports;
+  , tests    = exports
+  , couch
+  ;
+
+  couch = nock(cfg.url)
+    .put('/' + db_name('b'))
+    .reply(201, "{\"ok\":true}\n", 
+      { server: 'CouchDB/1.1.1 (Erlang OTP/R14B04)',
+        location: cfg.url + '/' + db_name('b'),
+        date: 'Fri, 02 Dec 2011 19:28:49 GMT',
+        'content-type': 'application/json',
+        'content-length': '12',
+        'cache-control': 'must-revalidate' })
+    .put('/' + db_name('a'))
+    .reply(201, "{\"ok\":true}\n", 
+      { server: 'CouchDB/1.1.1 (Erlang OTP/R14B04)',
+        location: cfg.url + '/' + db_name('a'),
+        date: 'Fri, 02 Dec 2011 19:28:49 GMT',
+        'content-type': 'application/json',
+        'content-length': '12',
+        'cache-control': 'must-revalidate' })
+    .put('/' + db_name('a') + '/foo', {"foo": "bar"})
+    .reply(201, "{\"ok\": true,\"id\": \"foo\",\"rev\": \"1-4c6114c65e295552ab1019e2b046b10e\"}\n", 
+      { server: 'CouchDB/1.1.1 (Erlang OTP/R14B04)',
+        location: cfg.url + '/' + db_name('a') + '/foo',
+        etag: '"1-4c6114c65e295552ab1019e2b046b10e"',
+        date: 'Fri, 02 Dec 2011 19:28:49 GMT',
+        'content-type': 'application/json',
+        'content-length': '66',
+        'cache-control': 'must-revalidate' })
+    .put('/' + db_name('b') + '/foo', {"foo": "bar"})
+    .reply(201, "{\"ok\": true,\"id\": \"foo\",\"rev\": \"1-4c6114c65e295552ab1019e2b046b10e\"}\n", 
+      { server: 'CouchDB/1.1.1 (Erlang OTP/R14B04)',
+        location: cfg.url + '/' + db_name('b') + '/foo',
+        etag: '"1-4c6114c65e295552ab1019e2b046b10e"',
+        date: 'Fri, 02 Dec 2011 19:28:49 GMT',
+        'content-type': 'application/json',
+        'content-length': '66',
+        'cache-control': 'must-revalidate' })
+    .get('/' + db_name('a') + '/foo')
+    .reply(200, "{\"_id\": \"foo\",\"_rev\": \"1-4c6114c65e295552ab1019e2b046b10e\",\"foo\": \"bar\"}\n", 
+      { server: 'CouchDB/1.1.1 (Erlang OTP/R14B04)',
+        etag: '"1-4c6114c65e295552ab1019e2b046b10e"',
+        date: 'Fri, 02 Dec 2011 19:28:50 GMT',
+        'content-type': 'application/json',
+        'content-length': '70',
+        'cache-control': 'must-revalidate' })
+    .put('/' + db_name('b') + '/foo', {"foo": "bar"})
+    .reply(409, 
+      "{\"error\": \"conflict\",\"reason\": \"Document update conflict.\"}\n", 
+      { server: 'CouchDB/1.1.1 (Erlang OTP/R14B04)',
+        date: 'Fri, 02 Dec 2011 19:28:50 GMT',
+        'content-type': 'application/json',
+        'content-length': '58',
+        'cache-control': 'must-revalidate' })
+    .get('/' + db_name('b') + '/foo?revs_info=true')
+    .reply(200, "{\"_id\": \"foo\",\"_rev\": \"1-4c6114c65e295552ab1019e2b046b10e\",\"foo\": \"bar\",\"_revs_info\": [{\"rev\":\"1-4c6114c65e295552ab1019e2b046b10e\",\"status\": \"available\"}]}\n", 
+      { server: 'CouchDB/1.1.1 (Erlang OTP/R14B04)',
+        date: 'Fri, 02 Dec 2011 19:28:50 GMT',
+        'content-type': 'application/json',
+        'content-length': '151',
+        'cache-control': 'must-revalidate' });
 
 function db(i) { return nano.use(db_name(i)); }
 
@@ -15,11 +77,11 @@ tests.get_doc = function (callback) {
 };
 
 tests.get_doc_ok = function (e,b) {
-  nano.db.destroy(db_name('a'));
-  this.t.notOk(e);
-  this.t.ok(b._rev);
-  this.t.equal(b._id, "foo");
-  this.t.equal(b.foo, "bar");
+  this.t.notOk(e, 'No excs');
+  this.t.ok(b._rev, 'I got rev');
+  this.t.equal(b._id, "foo", 'My id is foo');
+  this.t.equal(b.foo, "bar", 'My foo is bar');
+  this.t.ok(couch.isDone(), 'Nock is done');
 };
 
 tests.get_doc_params = function (callback) {
@@ -33,11 +95,11 @@ tests.get_doc_params = function (callback) {
 };
 
 tests.get_doc_params_ok = function (e,b) {
-  nano.db.destroy(db_name('b'));
-  this.t.notOk(e);
-  this.t.ok(b._revs_info);
-  this.t.equal(b._id, "foo");
-  this.t.equal(b.foo, "bar");
+  this.t.notOk(e, 'Err, not here');
+  this.t.ok(b._revs_info, 'Got revs info');
+  this.t.equal(b._id, "foo", 'Id is food');
+  this.t.equal(b.foo, "bar", 'Bar is in foo');
+  this.t.ok(couch.isDone(), 'Nock is done');
 };
 
 ensure(__filename,tests,module,process.argv[2]);
