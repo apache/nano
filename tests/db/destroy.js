acb79fe8fdce6c02be85f6ae1f2abec57002159e
Nuno Job <nunojobpinto@gmail.com>
2011-12-02 01:54:41 +0000

    [nock] db/destroy

--- tests/db/destroy.js
+++ tests/db/destroy.js
@@ -1,8 +1,26 @@
 var ensure   = require('ensure')
+  , nock     = require('nock')
   , cfg      = require('../../cfg/tests.js')
   , nano     = require('../../nano')(cfg)
   , db_name  = require('../utils').db_name("db_de")
-  , tests    = exports;
+  , tests    = exports
+  , couch
+  ;
+
+couch = nock(cfg.url)
+    .put('/' + db_name('1'))
+    .reply(201, "{\"ok\":true}\n", { server: 'CouchDB/1.1.1 (Erlang OTP/R14B04)',
+    location: cfg.url + '/' + db_name('1'),
+    date: 'Fri, 02 Dec 2011 01:50:45 GMT',
+    'content-type': 'application/json',
+    'content-length': '12',
+    'cache-control': 'must-revalidate' })
+    .delete('/' + db_name('1'))
+    .reply(200, "{\"ok\":true}\n", { server: 'CouchDB/1.1.1 (Erlang OTP/R14B04)',
+    date: 'Fri, 02 Dec 2011 01:50:46 GMT',
+    'content-type': 'application/json',
+    'content-length': '12',
+    'cache-control': 'must-revalidate' });
 
 tests.destroy_db = function (callback) {
   nano.db.create(db_name('1'), function () {
@@ -11,8 +29,9 @@ tests.destroy_db = function (callback) {
 };
 
 tests.destroy_db_ok = function (e,b) {
-  this.t.notOk(e);
-  this.t.equal(b.ok, true);
+  this.t.notOk(e, 'Error says Yey');
+  this.t.equal(b.ok, true, '{ok: yeah}');
+  this.t.ok(couch.isDone(), 'Nock not done');
 };
 
 ensure(__filename,tests,module,process.argv[2]);
