e761340f628b3249c9c047113b841cdeaf7a4208
Nuno Job <nunojobpinto@gmail.com>
2012-05-13 00:02:02 +0300

    [tests] db destroy

--- tests/db/destroy.js
+++ tests/db/destroy.js
@@ -1,37 +1,23 @@
-var ensure   = require('ensure')
-  , nock     = require('nock')
-  , cfg      = require('../../cfg/tests.js')
-  , nano     = require('../../nano')(cfg)
-  , db_name  = require('../utils').db_name("db_de")
-  , tests    = exports
-  , couch
+var specify  = require('specify')
+  , helpers  = require('../helpers')
+  , timeout  = helpers.timeout
+  , nano     = helpers.nano
+  , nock     = helpers.nock
   ;
 
-couch = nock(cfg.url)
-  .put('/' + db_name('1'))
-  .reply(201, "{\"ok\":true}\n", { server: 'CouchDB/1.1.1 (Erlang OTP/R14B04)',
-  location: cfg.url + '/' + db_name('1'),
-  date: 'Fri, 02 Dec 2011 01:50:45 GMT',
-  'content-type': 'application/json',
-  'content-length': '12',
-  'cache-control': 'must-revalidate' })
-  .delete('/' + db_name('1'))
-  .reply(200, "{\"ok\":true}\n", { server: 'CouchDB/1.1.1 (Erlang OTP/R14B04)',
-  date: 'Fri, 02 Dec 2011 01:50:46 GMT',
-  'content-type': 'application/json',
-  'content-length': '12',
-  'cache-control': 'must-revalidate' });
+var mock = nock(helpers.couch, "db/destroy");
 
-tests.destroy_db = function (callback) {
-  nano.db.create(db_name('1'), function () {
-    nano.db.destroy(db_name('1'), callback);
+specify("db_destroy:setup", timeout, function (assert) {
+  nano.db.create("db_destroy", function (err) {
+    assert.equal(err, undefined, "Failed to create database");
   });
-};
+});
 
-tests.destroy_db_ok = function (e,b) {
-  this.t.notOk(e, 'Error says Yey');
-  this.t.equal(b.ok, true, '{ok: yeah}');
-  this.t.ok(couch.isDone(), 'Nock not done');
-};
+specify("db_destroy:teardown", timeout, function (assert) {
+  nano.db.destroy("db_destroy", function (err) {
+    assert.equal(err, undefined, "Failed to destroy database");
+    assert.ok(mock.isDone(), "Some mocks didn't run");
+  });
+});
 
-ensure(__filename,tests,module,process.argv[2]);
+specify.run(process.argv.slice(2));
