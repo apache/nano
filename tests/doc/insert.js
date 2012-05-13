6f3f6b06530f548802cc84fda1990e488a47576f
Nuno Job <nunojobpinto@gmail.com>
2012-05-13 21:42:12 +0100

    [tests] removes insert2

--- tests/doc/insert.js
+++ tests/doc/insert.js
@@ -1,97 +1,62 @@
-var ensure   = require('ensure')
-  , nock     = require('nock')
-  , cfg      = require('../../cfg/tests.js')
-  , nano     = require('../../nano')(cfg)
-  , db_name  = require('../utils').db_name("doc_in")
-  , tests    = exports
-  , couch
+var specify  = require('specify')
+  , helpers  = require('../helpers')
+  , timeout  = helpers.timeout
+  , nano     = helpers.nano
+  , nock     = helpers.nock
   ;
 
-  couch = nock(cfg.url)
-    .put('/' + db_name('b'))
-    .reply(201, "{\"ok\":true}\n", 
-      { server: 'CouchDB/1.1.1 (Erlang OTP/R14B04)',
-        location: cfg.url + '/' + db_name('b'),
-        date: 'Fri, 02 Dec 2011 20:15:11 GMT',
-        'content-type': 'application/json',
-        'content-length': '12',
-        'cache-control': 'must-revalidate' })
-    .put('/' + db_name('a'))
-    .reply(201, "{\"ok\":true}\n", 
-      { server: 'CouchDB/1.1.1 (Erlang OTP/R14B04)',
-        location: 'http://nodejsbug.iriscouch.com/v061_doc_ina',
-        date: 'Fri, 02 Dec 2011 20:15:11 GMT',
-        'content-type': 'application/json',
-        'content-length': '12',
-        'cache-control': 'must-revalidate' })
-    .post('/' + db_name('a'), {"foo": "bar"})
-    .reply(201, "{\"ok\": true,\"id\": \"cb691f3e73482a0fb7e76bd3350037b4\",\"rev\": \"1-4c6114c65e295552ab1019e2b046b10e\"}\n", 
-      { server: 'CouchDB/1.1.1 (Erlang OTP/R14B04)',
-        location: cfg.url + '/' + db_name('a') +
-          '/cb691f3e73482a0fb7e76bd3350037b4',
-        date: 'Fri, 02 Dec 2011 20:15:11 GMT',
-        'content-type': 'application/json',
-        'content-length': '95',
-        'cache-control': 'must-revalidate' })
-    .put('/' + db_name('b') + '/some%2Fpath', {"foo": "bar", "fn": "function () { return true; }", fn2: "function () { return false; }"})
-    .reply(201, "{\"ok\": true,\"id\": \"some/path\",\"rev\": \"1-4c6114c65e295552ab1019e2b046b10e\"}\n", 
-      { server: 'CouchDB/1.1.1 (Erlang OTP/R14B04)',
-        location: cfg.url + '/' + db_name('b') + '/some/path',
-        etag: '"1-4c6114c65e295552ab1019e2b046b10e"',
-        date: 'Fri, 02 Dec 2011 20:15:11 GMT',
-        'content-type': 'application/json',
-        'content-length': '72',
-        'cache-control': 'must-revalidate' });
+var mock = nock(helpers.couch, "doc/insert")
+  , db   = nano.use("doc_insert")
+  ;
 
-function db(i) { return nano.use(db_name(i)); }
-
-tests.insert_doc = function (callback) {
-  nano.db.create(db_name("a"), function () {
-    db("a").insert({foo: "bar"}, callback);
+specify("doc_insert:setup", timeout, function (assert) {
+  nano.db.create("doc_insert", function (err) {
+    assert.equal(err, undefined, "Failed to create database");
   });
-};
+});
 
-tests.insert_doc_ok = function (e,b) {
-  this.t.notOk(e, 'No eerz');
-  this.t.ok(b.ok, 'This is ok');
-  this.t.ok(b.rev, 'I GOT REVZ');
-  this.t.ok(b.id, 'I got Id!');
-  this.t.ok(couch.isDone(), 'Nock is done');
-};
-
-tests.insert_doc_path = function (callback) {
-  nano.db.create(db_name("b"), function () {
-    db("b").insert({foo: "bar", fn: function () { return true; },
-      fn2: "function () { return false; }" }, 
-      'some/path', callback);
+specify("doc_insert:simple", timeout, function (assert) {
+  db.insert({"foo": "baz"}, "foobaz", function (error, foo) {   
+    assert.equal(error, undefined, "Should have stored foo");
+    assert.equal(foo.ok, true, "Response should be ok");
+    assert.ok(foo.rev, "Response should have rev");
   });
-};
+});
 
-tests.insert_doc_path_ok = function (e,b) {
-  this.t.notOk(e, 'Y U NO Errs');
-  this.t.ok(b.ok, 'I got ok');
-  this.t.ok(b.rev, 'I got rev');
-  this.t.equal(b.id, "some/path", 'I got id, not over 21 though');
-  this.t.ok(couch.isDone(), 'Nock is done');
-};
-
-tests.insert_doc_stream = function (callback) {
-  nano.db.create(db_name("c"), function () {
-    var buffer = "";
-    var foo = db('c').insert({"foo": "bar"});
-    foo.on('data', function(chunk) { buffer += chunk; });
-    foo.on('end', function () { callback(null, buffer); });
-    foo.on('error', callback);
+specify("doc_insert:functions", timeout, function (assert) {
+  db.insert({fn: function () { return true; },
+  fn2: "function () { return true; }"}, function (error, fns) {   
+    assert.equal(error, undefined, "Should have stored foo");
+    assert.equal(fns.ok, true, "Response should be ok");
+    assert.ok(fns.rev, "Response should have rev");
+    db.get(fns.id, function (error, fns) {
+      assert.equal(fns.fn, fns.fn2, "fn matches fn2");
+      assert.equal(error, undefined, "Should get foo");
+    });
   });
-};
+});
 
-tests.insert_doc_stream_ok = function (e,b) {
-  this.t.notOk(e, 'No error');
-  this.t.ok(b.ok, 'This is ok');
-  this.t.ok(b.rev, 'I GOT REVZ');
-  this.t.ok(b.id, 'I got Id!');
-  this.t.ok(couch.isDone(), 'Nock is done');
-};
+specify("doc_insert:streaming", timeout, function (assert) {
+  var buffer = ""
+    , foobar = db.insert({"foo": "bar"})
+    ;
 
+  function runAssertions(error, foobar) {
+    assert.equal(error, undefined, "Should have stored foobar");
+    assert.ok(foobar.ok, "This is ok");
+    assert.ok(foobar.rev, "I GOT REVZ");
+  }
 
-ensure(__filename,tests,module,process.argv[2]);
+  foobar.on('data', function(chunk) { buffer += chunk; });
+  foobar.on('end', function () { runAssertions(null, JSON.parse(buffer)); });
+  foobar.on('error', runAssertions);
+});
+
+specify("doc_insert:teardown", timeout, function (assert) {
+  nano.db.destroy("doc_insert", function (err) {
+    assert.equal(err, undefined, "Failed to destroy database");
+    assert.ok(mock.isDone(), "Some mocks didn't run");
+  });
+});
+
+specify.run(process.argv.slice(2));
