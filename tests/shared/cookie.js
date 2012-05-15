7b5d5e87c57a8bb54cc08a67a211d717f5e96ee5
Jonathan Mahoney <jonathan@websparkco.org>
2012-05-15 23:40:50 +0100

    Fixed naming convention mis-match in new cookie test

--- tests/shared/cookie.js
+++ tests/shared/cookie.js
@@ -9,14 +9,14 @@ var specify  = require("specify")
 var mock = nock(helpers.couch, "shared/cookie")
   ;
 
-specify("cookie:setup", timeout, function (assert) {
+specify("shared_cookie:setup", timeout, function (assert) {
   console.log()
   nano.db.create("shared_cookie", function (err) {
     assert.equal(err, undefined, "Failed to create database");
   });
 });
 
-specify("cookie:test", timeout, function (assert) {
+specify("shared_cookie:test", timeout, function (assert) {
   db = Nano({ url : "http://localhost:5984", cookie: "abc123" }).use("shared_cookie");
   db.insert({"foo": "bar"}, null, function (error, response) {
     assert.equal(response.request.headers['X-CouchDB-WWW-Authenticate'], "Cookie", 
@@ -26,7 +26,7 @@ specify("cookie:test", timeout, function (assert) {
   });
 });
 
-specify("cookie:teardown", timeout, function (assert) {
+specify("shared_cookie:teardown", timeout, function (assert) {
   nano.db.destroy("shared_cookie", function (err) {
     assert.equal(err, undefined, "Failed to destroy database");
     assert.ok(mock.isDone(), "Some mocks didn't run");
