59b6ebecfd2cfa06997a26e5a897561c7f17b192
Nuno Job <nunojobpinto@gmail.com>
2012-05-16 01:09:52 +0200

    [cookies] work in progress

--- tests/shared/cookie.js
+++ tests/shared/cookie.js
@@ -2,31 +2,34 @@ var specify  = require("specify")
   , helpers  = require("../helpers")
   , timeout  = helpers.timeout
   , Nano     = helpers.Nano
-  , nano     = helpers.nano
   , nock     = helpers.nock
+  , nano     = Nano({ url : helpers.couch, cookie: "abc123" })
   ;
 
 var mock = nock(helpers.couch, "shared/cookie")
+  , db   = nano.use("shared_cookie")
   ;
 
-specify("cookie:setup", timeout, function (assert) {
-  console.log()
-  nano.db.create("shared_cookie", function (err) {
-    assert.equal(err, undefined, "Failed to create database");
-  });
-});
-
-specify("cookie:test", timeout, function (assert) {
-  db = Nano({ url : "http://localhost:5984", cookie: "abc123" }).use("shared_cookie");
-  db.insert({"foo": "bar"}, null, function (error, response) {
-    assert.equal(response.request.headers['X-CouchDB-WWW-Authenticate'], "Cookie", 
-      "Request header 'X-CouchDB-WWW-Authenticate' was missing");
-    assert.equal(response.request.headers['cookie'], "abc123", 
+specify("shared_cookie:setup", timeout, function (assert) {
+  nano.db.create("shared_cookie", function (error, response, headers) {
+    console.log(headers)
+    assert.equal(error, undefined, "Failed to create database");
+    assert.equal(response.request.headers.cookie, "abc123", 
       "Cookie not sent in request headers");
   });
 });
 
-specify("cookie:teardown", timeout, function (assert) {
+specify("shared_cookie:test", timeout, function (assert) {
+  db.insert({"foo": "bar"}, null, function (error, response, headers) {
+    console.log(headers)
+    assert.equal(response.request.headers['X-CouchDB-WWW-Authenticate'],
+      "Cookie", "Request header 'X-CouchDB-WWW-Authenticate' was missing");
+    assert.equal(response.request.headers.cookie, "abc123", 
+      "Cookie not sent in request headers");
+  });
+});
+
+specify("shared_cookie:teardown", timeout, function (assert) {
   nano.db.destroy("shared_cookie", function (err) {
     assert.equal(err, undefined, "Failed to destroy database");
     assert.ok(mock.isDone(), "Some mocks didn't run");
