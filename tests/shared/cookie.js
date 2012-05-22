2f88c166ed0e696c05c0c18dacc40e2608b6475e
Jonathan Mahoney <jonathan@websparkco.org>
2012-05-22 18:27:59 +0100

    Cookie authentication
    Adds cookie authentication functionality detailed in issue #71, plus
    fixes issue with cookie jar in issue #73

--- tests/shared/cookie.js
+++ tests/shared/cookie.js
@@ -1,39 +1,47 @@
-var specify  = require("specify")
-  , helpers  = require("../helpers")
-  , timeout  = helpers.timeout
-  , Nano     = helpers.Nano
-  , nock     = helpers.nock
-  , nano     = Nano({ url : helpers.couch, cookie: "abc123" })
+var specify    = require("specify")
+  , helpers    = require("../helpers")
+  , timeout    = helpers.timeout
+  , nano       = helpers.nano
+  , Nano       = helpers.Nano
+  , nock       = helpers.nock
   ;
 
 var mock = nock(helpers.couch, "shared/cookie")
-  , db   = nano.use("shared_cookie")
+  ,  cookie
+  ,  cookie_nano
+  ,  nano_admin = new Nano('http://admin:password@localhost:5984')
   ;
 
 specify("shared_cookie:setup", timeout, function (assert) {
-  nano.db.create("shared_cookie", function (error, response, headers) {
-    console.log(headers)
-    assert.equal(error, undefined, "Failed to create database");
-    assert.equal(response.request.headers.cookie, "abc123", 
-      "Cookie not sent in request headers");
+  nano_admin.db.create("shared_cookie", function (err, response) {
+    assert.equal(err, undefined, "Failed to create database");
+        nano.request({
+            method: "POST",
+            db: "_session",
+            form: { "name" : "admin", "password" : "password" },
+            content_type: "application/x-www-form-urlencoded; charset=utf-8"
+          }, function (err, response, headers) {
+             assert.equal(err, undefined, "Should have logged in successfully");
+             assert.ok(headers['set-cookie'], "Response should have a set-cookie header");
+             cookie = headers['set-cookie'];
+        });
   });
 });
 
 specify("shared_cookie:test", timeout, function (assert) {
-  db.insert({"foo": "bar"}, null, function (error, response, headers) {
-    console.log(headers)
-    assert.equal(response.request.headers['X-CouchDB-WWW-Authenticate'],
-      "Cookie", "Request header 'X-CouchDB-WWW-Authenticate' was missing");
-    assert.equal(response.request.headers.cookie, "abc123", 
-      "Cookie not sent in request headers");
+  Nano({ url : "http://localhost:5984", cookie: cookie }).use("shared_cookie")
+  .insert({"foo": "baz"}, null, function (error, response) {
+    assert.equal(error, undefined, "Should have stored value");
+    assert.equal(response.ok, true, "Response should be ok");
+    assert.ok(response.rev, "Response should have rev");
   });
 });
 
 specify("shared_cookie:teardown", timeout, function (assert) {
-  nano.db.destroy("shared_cookie", function (err) {
-    assert.equal(err, undefined, "Failed to destroy database");
-    assert.ok(mock.isDone(), "Some mocks didn't run");
-  });
+    nano_admin.db.destroy("shared_cookie", function (err) {
+      assert.equal(err, undefined, "Failed to destroy database");
+      assert.ok(mock.isDone(), "Some mocks didn't run");
+    });
 });
 
 specify.run(process.argv.slice(2));
