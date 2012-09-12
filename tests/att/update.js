3a8e86b8446683439ab4c344f4d4e348fc51400d
Nuno Job <nunojobpinto@gmail.com>
2012-09-12 20:00:14 +0100

    [test] adds test to help user with #101

--- tests/att/update.js
+++ tests/att/update.js
@@ -4,6 +4,7 @@ var specify  = require('specify')
   , nano     = helpers.nano
   , nock     = helpers.nock
   , pixel    = helpers.pixel
+  , rev
   ;
 
 var mock = nock(helpers.couch, "att/update")
@@ -22,12 +23,24 @@ specify("att_update:test", timeout, function (assert) {
     function(error, hello) {
       assert.equal(error, undefined, "Should store hello");
       assert.equal(hello.ok, true, "Response should be ok");
-      assert.ok(hello.rev, "Should have a revision number");
       db.attachment.insert("new", "att", buffer, "image/bmp", 
       { rev: hello.rev }, function (error, bmp) {
         assert.equal(error, undefined, "Should store the pixel");
+        assert.ok(bmp.rev, "Should have a revision number");
+        rev = bmp.rev;
       });
     });
+});
+
+specify("att_update:metadata", timeout, function (assert) {
+  db.get("new", function (error, new_doc) {
+    assert.equal(error, undefined, "Should get new");
+    new_doc.works = true;
+    db.insert(new_doc, "new", function (error, response) {
+      assert.equal(error, undefined, "Should update doc");
+      assert.equal(response.ok, true, "Response should be ok");
+    });
+  })
 });
 
 specify("att_update:teardown", timeout, function (assert) {
